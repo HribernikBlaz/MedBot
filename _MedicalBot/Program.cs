83f6bdefb12ef34ce1873d188610212f0c3fe946
HribernikBlaz <hrib.blazhrib@gmail.com>
2024-02-09 23:47:12 +0100

    test

--- _MedicalBot/Program.cs
+++ _MedicalBot/Program.cs
@@ -24,6 +24,7 @@ namespace _MedicalBot
                     break;
                 }
                 Console.WriteLine($"Error: {errorMessage}");
+
                 Console.WriteLine("--------------------");
                 Console.Write("Enter Patient Name: ");
             } while (true);
